7039b21ad99423f9675f602d74bb540a683d3b20
Axthithya <aathithyavasanth@gmail.com>
2026-05-22 00:18:27 +0530

    Initial DevOps starter kit setup

---
+++ app/src/main/java/com/devopsstarter/app/HelloController.java
@@ -0,0 +1,17 @@
+package com.devopsstarter.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public Map<String, String> home() {
+        return Map.of(
+                "message", "DevOps Starter Kit Running"
+        );
+    }
+}
